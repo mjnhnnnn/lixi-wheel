9465e715cc2683f8050b8b060a1d441f6083f53b
mjnhnnnn <60811132+mjnhnnnn@users.noreply.github.com>
2026-02-16 13:22:58 +0700

    k

--- netlify/functions/get-prize.js
+++ netlify/functions/get-prize.js
@@ -1,9 +1,9 @@
 const prizes = [
-  { text: "10.000đ", prob: 48 },
-  { text: "20.000đ", prob: 48 },
-  { text: "100.000đ", prob: 3 },
-  { text: "500.000đ", prob: 0.8 },
-  { text: "5.000.000đ", prob: 0.2 }
+  { text: "10.000đ", prob: 49 },
+  { text: "20.000đ", prob: 49 },
+  { text: "100.000đ", prob: 1.97 },
+  { text: "500.000đ", prob: 0.03 }, // Cực hiếm (0.03%)
+  { text: "5.000.000đ", prob: 0 }    // Không thể trúng
 ];
 
 const totalProb = prizes.reduce((sum, p) => sum + p.prob, 0);
